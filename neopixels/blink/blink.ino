898be5ac540e4bb564a6cc2866613c27d045b295
uno seis tres <sharondelacruz@gmail.com>
2019-03-11 12:37:54 -0400

    Update blink.ino

--- neopixels/blink/blink.ino
+++ neopixels/blink/blink.ino
@@ -1,10 +1,12 @@
 #include <Adafruit_NeoPixel.h> // include neopixel library
 
-#define PIN 12 // Which pin on the Arduino is connected to the NeoPixels?
-#define NUMPIXELS 3 // How many NeoPixels are attached to the Arduino?
+int PIN 12 // Which pin on the Arduino is connected to the NeoPixels?
+int NUMPIXELS 3 // How many NeoPixels are attached to the Arduino?
+
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 
 // runs when you turn on the Flora
 void setup() {
